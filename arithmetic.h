722df3b42d883b0b1fb051446a44fd656bcbe835
salmanzafar123 <salmanmoinzafar@ymail.com>
2013-09-21 17:26:38 +0530

    Create arithmetic.h

---
+++ arithmetic.h
@@ -0,0 +1,11 @@
+#import<Foundation/Foundation.h>
+
+@interface arithmetic: NSObject
+
+-(int) addpara : (int)a: (int)b;
+-(int) multipara : (int)a: (int)b;
+-(int) subpara : (int)a: (int)b;
+-(float) divpara : (int)a: (int)b;
+
+
+@end
