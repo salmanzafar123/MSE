68b0113c89537e0f98534db0fa407ff146cb9083
salmanzafar123 <salmanmoinzafar@ymail.com>
2013-09-21 20:10:52 +0530

    Create arithmetic.m

---
+++ arithmetic.m
@@ -0,0 +1,25 @@
+#import "arithmetic.h"
+
+@implementation arithmetic: NSObject
+
+-(int) addpara : (int)a : (int)b
+{
+	return a+b;
+}
+
+-(int) multipara : (int)a : (int)b
+{
+	return a*b;
+}
+
+-(int) subpara : (int)a : (int)b
+{
+	return a-b;
+}
+
+-(float) divpara : (int)a: (int)b
+{
+	return a/b;
+}
+
+@end
